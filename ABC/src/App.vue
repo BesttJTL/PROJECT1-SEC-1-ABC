e8b358383a80e359301980691e782554276e78c2
MarkKMUTT <88104343+MarkKMUTT@users.noreply.github.com>
2023-02-01 23:18:13 +0700

    sample1

--- ABC/src/App.vue
+++ ABC/src/App.vue
@@ -1,4 +1,29 @@
 <script setup>
+//sample
+function randomQuestion() {
+  const questions = [
+    {
+      question: "Bat",
+      answer: "ค้างคาว",
+    },
+    {
+      question: "Japan",
+      answer: "ญี่ปุ่น",
+    },
+  ];
+  const randomIndex = Math.floor(Math.random() * questions.length);
+  return questions[randomIndex];
+}
+const correctAnswer = ref(0);
+function countCorrectAnswer() {
+  correctAnswer.value++;
+}
+
+function checkAnswer(answer) {
+  if (answer === randomQuestion().answer) {
+    countCorrectAnswer();
+  } 
+}
 
 </script>
  
