3711006b6d808843a686be559e03bde02de74660
Nattaphat <nattaphat.ngar@kmutt.ac.th>
2023-02-01 22:29:56 +0700

    2nd

--- ABC/src/App.vue
+++ ABC/src/App.vue
@@ -4,20 +4,44 @@
  
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline text-green-200">
-      Dev mode
-    </h1>
-
     <section class="navbar">
             <ul>
                 <li><a href="#">Level Select</a></li>
                 <li><a href="#">Credits</a></li>
             </ul>
         </section>
+    <h1 class="text-3xl font-bold underline text-green-200">
+      Dev mode
+    </h1>
+
   </div>
   
 </template>
  
 <style scoped>
+.navbar{
+    display:flex;
+    background-color: #333;
+    height: 5rem;
+}
+ul li{
+    display: flex;
+    list-style: none;
+    align-content: center;
+    margin:0px 0px 1rem 1rem;
+    justify-content: center;
+}
 
+ul{
+    display: flex;
+    align-content: center;
+    margin-top: 10px;
+    padding: 1rem;
+}
+
+a{
+    text-decoration: none;
+    display: flex;
+    color: #fff;
+}
 </style>
