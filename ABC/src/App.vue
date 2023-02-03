6a56eead1f0873bdf8a454eb5eb5853cc1db540c
MarkKMUTT <88104343+MarkKMUTT@users.noreply.github.com>
2023-02-03 23:03:33 +0700

    Button

--- ABC/src/App.vue
+++ ABC/src/App.vue
@@ -24,6 +24,7 @@
 //     countCorrectAnswer();
 //   } 
 // }
+
 </script>
  
 <template>
@@ -39,9 +40,19 @@
           <img src="./components/Characters/Blueman.gif" class="w-96 absolute bottom-44 left-8">
           <img src="./components/Characters/GreenSlime.gif" class="w-96  absolute bottom-44 right-14">
           <!-- ออกเเบบได้เลยครับผม -->
+          
           <div id="first" class="w-full h-1/3 bg-transparent">01</div>
           <div id="second" class="w-full h-1/3 bg-transparent">02</div>
-          <div id="third" class="w-full h-1/3 bg-transparent">03</div>
+          <div id="third" class="w-full h-1/3 bg-transparent flex items-center justify-center">
+            <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-8 px-20 border-4 border-blue-700 hover:border-blue-500 rounded mr-10">
+              Button1
+            </button>
+
+            <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-8 px-20 border-4 border-blue-700 hover:border-blue-500 rounded mr-10">
+              Button2
+            </button>
+          </div>
+        
         </div>
         <div id="right" class="w-1/12 h-full bg-blue-400">right</div>
       </div>
