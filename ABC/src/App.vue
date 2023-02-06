aa50aa760367c8e316172812def582e6dc92cd2b
BesttJTL <111588284+BesttJTL@users.noreply.github.com>
2023-02-06 20:40:23 +0700

    Update

--- ABC/src/App.vue
+++ ABC/src/App.vue
@@ -1,4 +1,29 @@
 <script setup>
+  import words from './data/words.json'
+  
+  function randomword(){
+
+    let word = words[Math.floor(Math.random() * words.length)]
+
+    const newword = words.filter(x => x != word)
+    // console.log(newword)
+    function showword(){
+      return word.English
+    }
+    function correctword(){
+      return word.Thai
+    }
+    function wrongword(){
+
+    }
+    return {
+      showword,
+      correctword,
+      wrongword
+    }
+  }
+  const { showword, correctword, wrongword } = randomword()
+ 
 
 </script>
  
@@ -17,23 +42,26 @@
         bg-[length:100%_100%] bg-[url('./src/components/BackgroundPicture/background.png')]">
           <img src="./components/Characters/Blueman.gif" class="w-64 absolute bottom-44 left-8">
           <img src="./components/Characters/GreenSlime.gif" class="w-64  absolute bottom-44 right-14">
-          <!-- ออกเเบบได้เลยครับผม -->
-          <div id="first" class="w-full h-1/3 bg-transparent flex justify-center content-center relative text-center">
-            <div></div>
-            <img src="./components/UIButton/question.png" alt="image" class="inset-0 w-80 object-cover">
-            <div class="flex absolute inset-0">dsaddasdadsadasa</div>
+
+          <div id="first" class="w-full h-1/3 flex justify-center relative"> 
+            <img src="./components/UIButton/question.png" class="absolute w-80 pt-2"/>
+            
+            
+            <div class="absolute top-36  text-white text-2xl font-medium">{{ showword() }}</div>
+            
           </div>
+          
           <div id="second" class="w-full h-1/3 bg-transparent">02</div>
-          <div id="third" class="w-full h-1/3 bg-transparent flex justify-center space-x-12">
+          <div id="third" class="w-full h-1/3 flex justify-center space-x-12">
                 <button class="relative">
                   <img src="./components/UIButton/ans2.png" alt="image" class="inset-0 w-60 object-cover ">
-                  <span class="absolute inset-0 w-auto  flex items-center justify-center text-white text-2xl font-medium  hover:underline">
-                    ทหาร
+                  <span class="absolute inset-0 w-auto  flex items-center justify-center text-white text-xl font-medium  hover:underline">
+                    {{ correctword() }}
                   </span>
                 </button>
                 
                 <button class="relative">
-                  <img src="./components/UIButton/ans1.png" alt="image" class="inset-0 w-60 object-cover">
+                  <img src="./components/UIButton/ans1.png" alt="image" class="inset-0 w-60 object-cover ">
                   <span class="absolute inset-0 w-auto flex items-center justify-center text-white text-2xl font-medium  hover:underline">
                     ตำรวจ
                   </span>
