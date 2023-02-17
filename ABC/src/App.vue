133c8c544fc0e91d258159640aeb6018606286f3
Tepathip <tepathippussaban@gmail.com>
2023-02-17 20:14:06 +0700

    playicon

--- ABC/src/App.vue
+++ ABC/src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 import word from './components/data/word.json'
-import { ref, reactive } from 'vue';
+import { ref, reactive, onMounted } from 'vue';
+import MajesticonsReload from './components/icons/MajesticonsReload.vue'
+import SystemUiconsVolumeDisabled from './components/icons/SystemUiconsVolumeDisabled.vue'
+import SystemUiconsVolumeHigh from './components/icons/SystemUiconsVolumeHigh.vue'
 const boy = 'images/maingif2.gif'
 const slime = 'images/slimegif.gif'
 const heart = 'images/huajai.png'
@@ -8,7 +11,7 @@ const logoIndex = 'images/logo.png'
 const attackBoy = 'images/dead.png'
 const boyAttack = 'images/mainatkgif.gif'
 const attackSlime = 'images/slimeded.png'
-
+const bgMusic = 'music/kirai.mp3'
 ////// static function
 const box = []
 const array = word
@@ -19,30 +22,13 @@ const hp = ref(3)
 const hideone = ref()
 const hidetwo = ref()
 const hidethree = ref()
+const overlay = ref(null)
+const boyRef = ref(null)
+const slimeRef = ref(null)
 random()
-
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-}
-
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
-
-// const countdown = setInterval(() => {
-//   if(time > 0){
-//     setTimeout(() => {
-//       if(time === 0){
-//         clearInterval()
-
-//       }
-//       time-=1
-//       countdown()
-//     })
-//   }
-// }, 1000)
-
-
+const showOverlay = (() =>{
+  overlay.value.style.width = '100%'
+})
 function random(){
   let randomobject = array[Math.floor(Math.random() * array.length)] //random words with specific range.
   box[0] = randomobject //keep randomed word at first position in new array
@@ -50,7 +36,6 @@ function random(){
   console.log(randomobject)
   return randomobject.English
 }
-
 function answerRandom(){
   let trueword = box[0] // right answer is in the first array index.
   let fakegroup = array.filter(x => x != trueword)  // pick wrong ans from the first element in array which is filtered by not select a correct answer in the array.
@@ -65,7 +50,6 @@ function answerRandom(){
     answer[1] = trueword.Thai
   }
   return answer
-
 }
 function leftanswer(x){
   return x[0]
@@ -73,30 +57,25 @@ function leftanswer(x){
 function rightanswer(x){
   return x[1]
 }
-
 //function ตัวละครโดนโจมตีเมื่อตอบผิด
 function changeImage() {
-  const imageElement = document.querySelector('img#boy');
-  imageElement.src = attackBoy;
-  setTimeout(function() {
-    imageElement.src = boy;
+  boyRef.value.src = attackBoy
+  setTimeout(() => {
+    boyRef.value.src = boy
   }, 1000);
 }
-
 //function ตัวละครไปโจมตีเมื่อตอบถูก
-function changeGIF() {
-  const imageElement = document.querySelector('img#boy');
-  imageElement.src = boyAttack;
-  setTimeout(function() {
-    imageElement.src = boy;
-  }, 1000);
-  const slimeElement = document.querySelector('img#slime');
-  slimeElement.src = attackSlime;
-  setTimeout(function() {
-    slimeElement.src = slime;
-  }, 1000);
-}
-
+  const changeGIF = () =>{
+      boyRef.value.src = boyAttack;
+      setTimeout(() => {
+        boyRef.value.src = boy;
+        }, 1000);
+  
+      slimeRef.value.src = attackSlime;
+      setTimeout(() => {
+      slimeRef.value.src = slime;
+        }, 1000);
+  }
 ////// addEvent
 function checkanswer(x){
   if(x === undefined){
@@ -130,45 +109,49 @@ function heartattack(){
     hidethree.value = {'visibility': 'hidden'}
   }
 }
-
-//consult how to reload
-const reload = () =>{
-  document.location.href='./'
+const reset = () =>{
+  location.reload()
 }
-
-//hide page fn
+const parent = ref(null)
+const fullScreenClass = ref(null)
 const hide =() =>{
-  let divParent = document.getElementById('parent')
-  let divContent = document.getElementById('fullscreen')
-  divParent.remove(divParent)
-  divContent.className='flex flex-col w-screen h-screen'
+  parent.value.remove(parent)
+  fullScreenClass.value.className = 'flex flex-col w-screen h-screen'
 } 
+
+//song toggle
+const bgSong = ref('bgsong.mp3')
+const playing = ref(true)
+const song = ref(null)
+const playSong = () => {
+  if(playing.value) song.value.play()
+  else song.value.pause()
+  playing.value = !playing.value
+}
+const play = SystemUiconsVolumeDisabled
 </script>
 
 <template>
-  <!-- index -->
-  <div class="w-screen h-screen flex flex-col items-center justify-center gap-y-8 bg-[url('images/background2b.png')] bg-[length:100%_100%] bg-center" id="parent">
-    <div class="flex pb-10">
-      <h1 class="text-8xl text-white text-bold">Game A B C</h1>
-    </div>
-      <div class="flex pb-32">
-        <a href="#fullscreen" class="btn justify-center items-center" @click="hide" id="btn">Let's Play</a>
+  <div id="mainDiv">
+    <div class="w-screen h-screen flex flex-col items-center justify-center gap-y-8 bg-[url('images/background2b.png')] bg-[length:100%_100%] bg-center" id="parent" ref="parent" >
+      <div class="flex pb-10">
+        <h1 class="text-8xl text-white text-bold">Game A B C</h1>
       </div>
-  </div>
+        <div class="flex pb-32">
+          <a href="#fullscreen" class="btn justify-center items-center" @click="hide" id="btn">Let's Play</a>
+        </div>
+    </div>
 
-
-  <!-- game page -->
-    <div id="fullscreen" class="hidden">
-      <!-- <div id="top" class="h-1/6 w-full bg-lime-200">Header</div> -->
-        <!-- <div id="left" class="w-1/12 h-full bg-blue-200">left</div> -->
-        <div id="background" class="flex flex-col w-full h-full relative bg-scroll bg-[length:100%_100%] bg-[url('/images/background.png')]">
-          <img id="boy" :src="boy" class="w-96 absolute bottom-52 left-8">
-          <img id="slime" :src="slime" class="w-80  absolute bottom-52 right-32">
-          <div class="absolute flex flex-row space-x-3 mt-7 ml-3  h-auto w-auto ">
+    <div id="fullscreen" class="hidden" ref="fullScreenClass" :class="hideScreen">
+        <div id="background" class="flex flex-col w-full h-full relative bg-[length:100%_100%] bg-[url('/images/background.png')] bg-fixed">
+          <img id="boy" ref="boyRef" :src="boy" class="w-80 absolute bottom-52 left-8 ">
+          <img id="slime" ref="slimeRef" :src="slime" class="w-80 absolute bottom-52 right-32 ">
+          <div class="absolute flex flex-row space-x-3 mt-7 ml-3  h-auto w-auto justify-start">
           <img :src="heart" class="w-10 " :style="hidethree"/>
           <img :src="heart" class="w-10 " :style="hidetwo"/>
           <img :src="heart" class="w-10 " :style="hideone"/>
           </div>
+          
           <div id="first" class="w-full h-1/3 flex flex-row box-border content-center items-center justify-around">
             <!-- Score -->
             <div id="score" class="bg-white border-4 border-black box-border w-32 h-18 mt-5 self-start text-center">
@@ -179,9 +162,14 @@ const hide =() =>{
               <p class="pt-8 duration-700" >{{ box[0].English }}</p>
             </div>
             <!-- button pause -->
-              <button @click="play()" class="w-14 h-14 self-start justify-self-end mt-5 rounded-full bg-blue-500 focus:outline-none" id="pause">
-                <font-awesome-icon icon="pause"  id="play-btn" class="fa-2x text-white text-center items-center content-center" />
-              </button>
+                <!-- <font-awesome-icon icon="reset" id="reset-btn" class="fa-2x text-white text-center items-center content-center" /> -->
+                <div class="bg-white border-4 border-black box-border w-20 h-20 rounded-full mt-3 self-start text-center">
+                  <audio ref="song" :src="bgSong" id="music"></audio>
+                  <button class="content-center item-center pt-1" @click="playSong">
+                    <p v-if="playing == true"><SystemUiconsVolumeDisabled/></p>
+                    <p v-else="playing == false"><SystemUiconsVolumeHigh/></p>
+                  </button>
+                </div>
           </div>
           <div id="second" class="w-full h-1/3 flex flex-col place-items-end">
             <!-- Countdown Timer -->
@@ -192,7 +180,7 @@ const hide =() =>{
             </div> -->
             </div>
           </div>
-          <div id="third" class="w-full h-1/3  flex justify-center items-center space-x-40">
+          <div id="third" class="w-full h-1/3  flex justify-center items-center space-x-28">
             <button class="relative bg-white border-4 border-black box-border w-64 h-2/5 text-center text-2xl bg-center duration-300 hover:bg-slate-300" 
             @click="checkanswer(answer[0])">
             {{ leftanswer(answer) }}
@@ -204,25 +192,89 @@ const hide =() =>{
           </div>
         </div>
         <!-- <div id="right" class="w-1/12  h-full bg-blue-400">right</div> -->
-      <div id="myNav" class="overlay" :class="hp === 0 ? openNav():''">
+      <div id="myNav" class="overlay" :class="hp === 0 ? showOverlay():''" ref="overlay">
           <div class="overlay-content flex flex-col justify-center content-center">
             <div class="overlay-element block mt-80 gap-5 w-full h-full pt-4 ">
               <h1 class="text-red-500 text-4xl text-center flex justify-center">Game Over!</h1>
-              <button class="border-2 border-blue-400 text-white p-3 my-6 rounded-md text-xl font-bold" @click="reload">GO TO FIRST PAGE</button>
+              <button class="border-2 border-blue-400 text-white p-3 my-6 rounded-md text-xl font-bold" @click="reset">GO TO FIRST PAGE</button>
           </div>
         </div>
       </div>
-       </div>       
+       </div>   
+
+       <!-- Start Responsive Div Zone -->
+       <!-- <div class="answerBtn flex flex-col w-36 h-full gap-y-4 align-center m-auto">
+        <button class="relative bg-white border-4 border-black box-border w-64 h-2/5 text-center text-2xl bg-center duration-300 hover:bg-slate-300">Answer Dummy 1</button>
+        <button class="relative bg-white border-4 border-black box-border w-64 h-2/5 text-center text-2xl bg-center duration-300 hover:bg-slate-300">Answer Dummy 2</button>
+       </div> -->
+
+  </div>
 </template>
 
 <style scoped>
-/* html, body {margin: 0; height: 100%; overflow-y: hidden} */
+/* Responsive Design */
+@media only screen and (max-width: 640px) {
+  button{
+    margin-top: 4rem;
+    padding: 0.5rem;
+    height: 7rem;
+    width: 7rem;
+    word-wrap: break-word;
+  }
+  #score{
+    margin-top: 5rem;
+    margin-left: 10px;
+  }
+  #word {
+    /* position: relative;
+    margin-left: rem;
+    margin-top: 10rem; */
+    margin:auto;
+    width:50%;
+    padding:10px;
+    /* width:13rem; */
+    word-wrap: break-word;
+  }
+  #pause{
+    visibility: visible;
+  }
+  #boy{
+    width: 10rem;
+    margin-left: 0;
+  }
+  #slime{
+    width:10rem;
+  }
+  /* End of Responsive Design */
+}
+html{
+  height: 100%;
+}
+body{
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: center center;
+  height:100%
+}
+#reset{
+  display:flex;
+  padding:2rem;
+  align-content: center;
+  justify-content: center;
+}
+#reset:hover{
+  background-color:yellow;
+  transition:width 0.3s ease-in-out;
+  width:10rem;
+}
+#reset:hover::after{
+  content: "Replay";
+}
 *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     font-family: 'Mitr', sans-serif;
-
 }
 body {
     display: flex;
@@ -266,7 +318,6 @@ a {
     );
     transition: all 650ms;
   }
-
   #btn:hover:before {
     left: 100%;
   }
